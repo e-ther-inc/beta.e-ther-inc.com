36f5ed61d4bf737a054e8d4e851b00768cf22453
0-vortex <0-vortex@users.noreply.github.com>
2021-08-25 17:02:27 +0000

    feat: add @carbon/react and @open-sauced/semantic-release-conventional-config c12181b139c94a53d249cfb06ca1c05e015f5975

---
+++ _next/static/51XhrW8Uq_6BkQXmd0SwL/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/739-d444f46faa56a05070c3.js","static/css/6fceadb801d1dda4d4ca.css","static/chunks/pages/index-ac72b362312341654b0b.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
