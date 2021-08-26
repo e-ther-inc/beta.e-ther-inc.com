c31ba8363ac638f344abd9e4e5241b5898b39884
0-vortex <0-vortex@users.noreply.github.com>
2021-08-26 22:06:23 +0000

    fix: static build generation (#2)
    * build: add proper husky configuration
    
    * fix: correct static export and docker requirements
    
    * docs: add workflow badges 88f07e976c65abf07e3e74ce634af7eafc882dcf

---
+++ _next/static/hhYWbYvKk0y4ap9cn_o-z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/739-d444f46faa56a05070c3.js","static/css/6fceadb801d1dda4d4ca.css","static/chunks/pages/index-e55eefad12a00d81ecd1.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
